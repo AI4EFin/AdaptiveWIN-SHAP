317730b92e13375f12cace10c2d1bd1aa965f6ef
Raul Bag <raulbag1996@gmail.com>
2025-10-05 17:38:40 +0300

    fix: increase more

--- examples/lstm_simulation.py
+++ examples/lstm_simulation.py
@@ -79,5 +79,5 @@ if __name__ == "__main__":
     NUM_BOOTSTRAP = 1
 
     results = cd.detect(min_window=MIN_SEG, n_0=N_0, jump=JUMP, search_step=STEP, alpha=ALPHA, num_bootstrap=NUM_BOOTSTRAP,
-                        t_workers=4, b_workers=4, one_b_threads=3)
+                        t_workers=12, b_workers=12, one_b_threads=1)
     print(f"Saved results to: {out_csv}")
